caf23ef034799cfd989fcccea2f1ccbb665829d0
y30046185 <yefan19@huawei.com>
2024-06-13 14:50:23 +0800

    modify proxy

--- lua/custom/autocmds.lua
+++ lua/custom/autocmds.lua
@@ -1,8 +1,13 @@
-local defualt_proxy = 'http://localhost:7890'
+local default_proxy = 'http://localhost:7890'
+local function mask_password(url)
+  return string.gsub(url, "(https?://.-:)(.-)(@)", "%1********%3")
+end
+
 local function set_proxy()
-  vim.env.http_proxy = defualt_proxy
-  vim.env.https_proxy = defualt_proxy
-  vim.env.all_proxy = defualt_proxy
+  vim.env.http_proxy = default_proxy
+  vim.env.https_proxy = default_proxy
+  vim.env.all_proxy = default_proxy
+  vim.notify("Proxy has been set to: " .. mask_password(default_proxy), vim.log.levels.INFO)
 end
 
 -- Function to unset proxy
@@ -10,10 +15,11 @@ local function unset_proxy()
   vim.env.http_proxy = nil
   vim.env.https_proxy = nil
   vim.env.all_proxy = nil
+  vim.notify("Proxy has been unset.", vim.log.levels.INFO)
 end
 
 set_proxy()
 
 -- Create commands to set and unset proxy
-vim.api.nvim_create_user_command('SetProxy', set_proxy, {})
-vim.api.nvim_create_user_command('UnsetProxy', unset_proxy, {})
+vim.api.nvim_create_user_command("SetProxy", set_proxy, {})
+vim.api.nvim_create_user_command("UnsetProxy", unset_proxy, {})
